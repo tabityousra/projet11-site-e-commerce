aade73dbc955e94324a5d1f63bcc7c81c2e31299
tabityousra <92039920+tabityousra@users.noreply.github.com>
2022-04-26 12:06:33 +0000

    ADD

--- live_coding/index.php
+++ live_coding/index.php
@@ -17,5 +17,16 @@
         </nav>
     </header>
     <main>
+
+    <div class="card" style="width: 18rem;">
+  <img src="shopping.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Produit 1</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Details</a>
+  </div>
+</div>
+
+    </main>
 </body>
 </html>
